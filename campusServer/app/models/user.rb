967982c0cf8777ffb1e43e34ec1eb59df8611db8
mfioravan <mfiroava@ycp.edu>
2015-10-24 20:58:19 -0400

    Added specific format returns

--- campusServer/app/models/user.rb
+++ campusServer/app/models/user.rb
@@ -46,7 +46,7 @@ class User < ActiveRecord::Base
     # Returns the hash digest of the given string.
    # def digest(string)
     #  cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
+                                                  #BCrypt::Engine.cost
       #BCrypt::Password.create(string, cost: cost)
     #end
 

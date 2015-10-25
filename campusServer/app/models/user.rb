c99111a054d498f975825544ab9dcc260ab79ce3
Tyler <tyounkin@ycp.edu>
2015-10-24 22:04:08 -0400

    Allow editing the user profiles

--- campusServer/app/models/user.rb
+++ campusServer/app/models/user.rb
@@ -6,10 +6,8 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
-  validates :password, presence: true
-
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   has_many :microposts
 
@@ -46,7 +44,7 @@ class User < ActiveRecord::Base
     # Returns the hash digest of the given string.
    # def digest(string)
     #  cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
+                                                 # BCrypt::Engine.cost
       #BCrypt::Password.create(string, cost: cost)
     #end
 

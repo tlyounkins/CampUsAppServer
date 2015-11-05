6b07cc1dabc0315963eb4322f3d65d2470b01c9f
Mfioravan <mfioravan@gmail.com>
2015-11-04 19:30:06 -0500

    Added Creation/Deletion/Showing of groups

--- campusServer/app/controllers/groups_controller.rb
+++ campusServer/app/controllers/groups_controller.rb
@@ -1,4 +1,56 @@
 class GroupsController < ApplicationController
-  def new
+  skip_before_filter :verify_authenticity_token
+
+  # GET /groups/1
+  # GET /groups/1.json
+  def show
+    @group = Groups.find(params[:id])
+    respond_to do |format|
+      format.html {}
+      format.json {render :json=>{:args => {:groupname => @group.groupname, :description => @group.description}}}
+    end
   end
+
+  # GET /groups/new
+  # GET /groups/new.json
+  def new
+    @group = Groups.new
+  end
+
+  # POST /groups
+  # POST /groups.json
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      @group.errors.full_messages
+      flash[:success] = "Group Created!"
+      respond_to do |format|
+        format.html {redirect_to @user}
+        format.json {render :json =>{ :success => true}}
+      end
+    else
+      @group.errors.full_messages
+      respond_to do |format|
+        format.html {render 'new'}
+        format.json {render :json => {:success =>false}}
+      end
+    end
+  end
+
+  # DELETE /groups/1
+  # DELETE /groups/1.json
+  def destroy
+    @group.destroy
+    respond_to do |format|
+      format.html { }#redirect_to users_url, notice: 'Group was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def group_params
+      params.require(:group).permit(:groupname, :description)
+    end
+
 end
--- campusServer/config/routes.rb
+++ campusServer/config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   delete 'logout'  => 'sessions#destroy'
   resources :users
   resources :microposts
+  resources :groups
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

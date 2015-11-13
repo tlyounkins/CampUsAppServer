82c30573f2f4b68d36d3b3877b699e3b189bbfc0
Tyler <tyounkin@ycp.edu>
2015-11-13 00:05:08 -0500

    Password Reset

--- campusServer/config/routes.rb
+++ campusServer/config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   delete 'logout'  => 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts
   resources :groups
 
--- campusServer/db/schema.rb
+++ campusServer/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151112184810) do
+ActiveRecord::Schema.define(version: 20151113044714) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "groupname",   limit: 255
@@ -52,6 +52,8 @@ ActiveRecord::Schema.define(version: 20151112184810) do
     t.string   "activation_digest", limit: 255
     t.boolean  "activated",                     default: false
     t.datetime "activated_at"
+    t.string   "reset_digest",      limit: 255
+    t.datetime "reset_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
